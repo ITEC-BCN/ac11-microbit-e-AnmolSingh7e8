3eede0c800ca828c9506c980f952df2fbc7ec7cd
Anmolpreet Singh Kaur <anmolpreet.singh.7e8@itb.cat>
2025-11-10 23:19:24 +0100

    Codi Final

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-valor_actual = input.temperature()
+
 valor_maxim = 50
 
-def on_button_pressed_a():
-    led.plot(valor_actual, valor_maxim)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def dibuxar_barra(valor_actual, valor_maxim):
+    alcada = int(valor_actual*5/valor_maxim)
+    if alcada < 0:
+        alacada = 0
+    if alcada > 5:
+        alcada = 5
+    led.stop_animation()
+
+    for x in range(alcada):
+        for y in range(5):
+            led.plot(x, 4-y)
+
+#Per fer funcionar el Repte 6 descomentar aquest codi i comentar el següent
+#while True:
+#    temp = input.temperature()
+#    dibuxar_barra(temp, valor_maxim)
+#    basic.pause(500)
+ 
+
+x = 2   
+y = 2  
+inclinacio = 150  
+
+while True:
+    led.plot(x, y)        
+    basic.pause(50)
+    led.unplot(x, y)   
+  
+    
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    
+
+    if accX <= -inclinacio and x > 0:   
+        x -= 1
+    if accX >= inclinacio and x < 4:   
+        x += 1
+    
+    if accY <= -inclinacio and y > 0:  
+        y -= 1
+    if accY >= inclinacio and y < 4:   
+        y += 1
--- main.ts
+++ main.ts
@@ -1,5 +1,53 @@
-let valor_actual = input.temperature()
+let accX: number;
+let accY: number;
 let valor_maxim = 50
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    led.plot(valor_actual, valor_maxim)
-})
+function dibuxar_barra(valor_actual: number, valor_maxim: number) {
+    let alacada: number;
+    let alcada = Math.trunc(valor_actual * 5 / valor_maxim)
+    if (alcada < 0) {
+        alacada = 0
+    }
+    
+    if (alcada > 5) {
+        alcada = 5
+    }
+    
+    led.stopAnimation()
+    for (let x = 0; x < alcada; x++) {
+        for (let y = 0; y < 5; y++) {
+            led.plot(x, 4 - y)
+        }
+    }
+}
+
+// Per fer funcionar el Repte 6 descomentar aquest codi i comentar el següent
+// while True:
+//     temp = input.temperature()
+//     dibuxar_barra(temp, valor_maxim)
+//     basic.pause(500)
+let x = 2
+let y = 2
+let inclinacio = 150
+while (true) {
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    if (accX <= -inclinacio && x > 0) {
+        x -= 1
+    }
+    
+    if (accX >= inclinacio && x < 4) {
+        x += 1
+    }
+    
+    if (accY <= -inclinacio && y > 0) {
+        y -= 1
+    }
+    
+    if (accY >= inclinacio && y < 4) {
+        y += 1
+    }
+    
+}
