6ac1639b2c7e10be88e58632fd3ba16e64b71513
Anmolpreet Singh Kaur <anmolpreet.singh.7e8@itb.cat>
2025-11-10 19:10:56 +0100

    Agafem la temperatura

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+valor_actual = input.temperature()
+valor_maxim = 50
 
+def on_button_pressed_a():
+    led.plot(valor_actual, valor_maxim)
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+let valor_actual = input.temperature()
+let valor_maxim = 50
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    led.plot(valor_actual, valor_maxim)
+})
